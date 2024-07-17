9e381a9b370cda5d528c2985c9a90f8de2dc1063
Vatambeduniharika <153808070+Vatambeduniharika@users.noreply.github.com>
2024-07-17 12:09:35 +0530

    Add files via upload

---
+++ 15-7-24(for loop programs).py
@@ -0,0 +1,64 @@
+#date---->15/07/24
+#program to iterate any iterable
+'''
+str_='python'
+for i in str_:
+    print(i)
+'''
+#another method
+'''
+n=['python','java','perl','css','html']
+for i in n:
+    print(i)
+'''
+#date---->16/07/24
+'''
+dict_={'python':56,'java':56.8,'perl':45,'css':True,45:'int'}
+for item in dict_:
+    print(item)
+'''
+'''
+dict_={'python':56,'java':56.8,'perl':45,'css':True,45:'int'}
+for key in dict_:
+    print(key)
+'''
+#unpack
+'''
+dict_={'python':56,'java':56.8,'perl':45,'css':True,45:'int'}
+for i in dict_.items():
+    print(i[0],i[1])
+'''
+#unpack another way
+'''
+dict_={'python':56,'java':56.8,'perl':45,'css':True,45:'int'}
+for k,v in dict_.items():
+    print(k,v)
+'''
+'''
+dict_={'python':56,'java':56.8,'perl':45,'css':True,45:'int'}
+for value in dict_:
+    print(value)
+'''
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
